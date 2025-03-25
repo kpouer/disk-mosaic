d3f763b6cc6f18f6cab7c03af0230bdcf87070c7
Matthieu Casanova <public@kpouer.com>
2025-03-25 23:25:47 +0100

    add size

--- src/ui/data_widget.rs
+++ src/ui/data_widget.rs
@@ -1,8 +1,9 @@
+use crate::data::Data;
 use eframe::epaint::FontFamily::Proportional;
 use eframe::epaint::FontId;
 use egui::{Color32, Pos2, Rect, Ui, Widget};
-
-use crate::data::Data;
+use humansize::DECIMAL;
+use treemap::Mappable;
 
 pub struct DataWidget<'a> {
     data: &'a Data,
@@ -15,6 +16,9 @@ impl<'a> DataWidget<'a> {
 }
 
 const HOVER_ZOOMING: f32 = 10.0;
+const FONT_SIZE: f32 = 18.0;
+const LABEL_COLOR: Color32 = Color32::WHITE;
+const FONT: FontId = FontId::new(FONT_SIZE, Proportional);
 
 impl Widget for DataWidget<'_> {
     fn ui(self, ui: &mut Ui) -> egui::Response {
@@ -45,29 +49,43 @@ impl Widget for DataWidget<'_> {
 }
 
 impl DataWidget<'_> {
-    fn draw_label(self, ui: &mut Ui, mut rect: Rect, zoomed: bool) {
-        let font_id = FontId::new(18.0, Proportional);
+    fn draw_label(&self, ui: &mut Ui, mut rect: Rect, zoomed: bool) {
         if zoomed {
             rect = rect.shrink(HOVER_ZOOMING);
         }
 
-        let layout = ui.painter().layout(
-            self.data.file_name().to_string(),
-            font_id,
-            Color32::BLACK,
-            ui.available_width(),
+        let galley_name = ui.painter().layout(
+            self.data.file_name().into(),
+            FONT,
+            LABEL_COLOR,
+            ui.available_width() - ui.spacing().item_spacing.x * 2.0,
         );
-        if layout.rect.width() < rect.width() {
+        if galley_name.rect.width() < rect.width() {
             ui.put(
-                Rect::from_min_max(
-                    rect.min,
-                    Pos2::new(
-                        rect.min.x + layout.rect.width(),
-                        rect.min.y + layout.rect.height(),
-                    ),
+                Rect::from_min_size(
+                    rect.min + ui.spacing().item_spacing,
+                    galley_name.rect.size(),
                 ),
-                egui::Label::new(layout),
+                egui::Label::new(galley_name),
             );
+            let galley_size = ui.painter().layout(
+                humansize::format_size(self.data.size() as u64, DECIMAL),
+                FONT,
+                LABEL_COLOR,
+                ui.available_width(),
+            );
+            if galley_size.rect.width() < rect.width() {
+                ui.put(
+                    Rect::from_min_max(
+                        Pos2::new(
+                            rect.min.x + ui.spacing().item_spacing.x,
+                            rect.max.y - galley_size.rect.height() - ui.spacing().item_spacing.y,
+                        ),
+                        Pos2::new(rect.min.x + galley_size.rect.width(), rect.min.y),
+                    ),
+                    egui::Label::new(galley_size),
+                );
+            }
         }
     }
 }
