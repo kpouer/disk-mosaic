1a2b16782825822e2a7d5fa82d388bdf6a8d3c05
Matthieu Casanova <chocolat.mou@gmail.com>
2025-04-10 08:36:27 +0200

    fix button position

--- src/ui/about_dialog.rs
+++ src/ui/about_dialog.rs
@@ -1,8 +1,10 @@
-use egui::{Button, Context, Image, include_image};
+use egui::{Button, Context, Image, Pos2, Rect, Vec2, include_image};
 
 pub(crate) struct AboutDialog<'a> {
     open: &'a mut bool,
 }
+
+const BUTTON_SIZE: f32 = 18.0;
 
 impl<'a> AboutDialog<'a> {
     pub(crate) fn new(open: &'a mut bool) -> Self {
@@ -10,12 +12,19 @@ impl<'a> AboutDialog<'a> {
     }
 
     pub(crate) fn show_button(&mut self, ctx: &Context, ui: &mut egui::Ui) {
-        ui.add_space(ui.available_width() - 18.0);
+        ui.add_space(ui.available_width() - BUTTON_SIZE);
+        let max_rect = ui.max_rect();
         if ui
-            .add(Button::image(
-                Image::from(include_image!("../../assets/question-mark.svg"))
-                    .tint(ctx.style().visuals.widgets.noninteractive.text_color()),
-            ))
+            .put(
+                Rect::from_min_size(
+                    Pos2::new(max_rect.right() - BUTTON_SIZE, max_rect.top()),
+                    Vec2::new(BUTTON_SIZE, BUTTON_SIZE),
+                ),
+                Button::image(
+                    Image::from(include_image!("../../assets/question-mark.svg"))
+                        .tint(ctx.style().visuals.widgets.noninteractive.text_color()),
+                ),
+            )
             .clicked()
         {
             *self.open = true;
