c5bdebf258949b2db5103e16840b913f8f598723
Matthieu Casanova <chocolat.mou@gmail.com>
2025-04-11 22:38:12 +0200

    simplify end of scan

--- src/ui/app_state/analyzer.rs
+++ src/ui/app_state/analyzer.rs
@@ -10,7 +10,7 @@ use log::info;
 use std::ops::{Add, AddAssign};
 use std::path::PathBuf;
 use std::sync::Arc;
-use std::sync::atomic::AtomicBool;
+use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::mpsc::Receiver;
 use std::thread;
 use std::time::Duration;
@@ -21,7 +21,6 @@ pub enum Message {
     Data(Data),
     DirectoryScanStart(String),
     DirectoryScanDone(ScanResult),
-    Finished,
 }
 
 #[derive(Debug, Default, Clone)]
@@ -58,7 +57,7 @@ pub struct Analyzer {
     pub(crate) analysis_result: AnalysisResult,
     rx: Receiver<Message>,
     stopper: Arc<AtomicBool>,
-    handle: Option<thread::JoinHandle<()>>,
+    handle: thread::JoinHandle<()>,
     scanning: String,
     scanned_directories: u64,
     scan_result: ScanResult,
@@ -73,12 +72,11 @@ impl Analyzer {
         let stopper = Arc::new(AtomicBool::new(false));
         let root_copy = root.clone();
         let stopper_copy = stopper.clone();
-        let handle = Some(thread::spawn(move || {
+        let handle = thread::spawn(move || {
             let start = std::time::Instant::now();
             Task::scan_directory_channel(&root_copy, &tx, &stopper_copy);
-            tx.send(Message::Finished).unwrap();
             info!("Done in {}ms", start.elapsed().as_millis());
-        }));
+        });
         Self {
             analysis_result: AnalysisResult::new(vec![Data::new_directory(&root)]),
             rx,
@@ -92,8 +90,7 @@ impl Analyzer {
     }
 
     pub(crate) fn show(&mut self, ctx: &Context) -> bool {
-        let finished = self.receive_data();
-
+        self.receive_data();
         if let Some(index) = self.show_top_panel(ctx) {
             self.analysis_result.selected_index(index);
         }
@@ -102,11 +99,10 @@ impl Analyzer {
             TreeMapPanel::new(&mut self.analysis_result).show(ui);
         });
         ctx.request_repaint_after(Duration::from_millis(60));
-        finished
+        self.handle.is_finished()
     }
 
-    fn receive_data(&mut self) -> bool {
-        let mut finished = false;
+    fn receive_data(&mut self) {
         for message in self.rx.try_iter() {
             match message {
                 Message::DirectoryScanStart(d) => {
@@ -122,42 +118,27 @@ impl Analyzer {
                         }
                     }
                 }
-                Message::Finished => {
-                    info!("Scan finished");
-                    finished = true;
-                }
             }
         }
-        finished
     }
 
     fn show_top_panel(&mut self, ctx: &Context) -> Option<usize> {
         let mut clicked_index = None;
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
             ui.horizontal(|ui| {
-                if let Some(handle) = &self.handle {
-                    if handle.is_finished() {
-                        self.handle = None;
-                    }
+                if ui.button("Stop").clicked() {
+                    info!("Stop requested");
+                    self.stopper.store(true, Ordering::Relaxed);
                 }
-                if self.handle.is_some() {
-                    if ui.button("Stop").clicked() {
-                        self.stopper
-                            .store(true, std::sync::atomic::Ordering::Relaxed);
-                    }
-                    ui.label(format!("Scanning: {}", self.scanning));
-                } else {
-                    if let Some(index) = PathBar::new(&self.analysis_result.data_stack).show(ui) {
-                        clicked_index = Some(index);
-                    }
-                    let scanning_label = Label::new(format!(
-                        "Directories: {}, Files: {}, Volume {}",
-                        self.scanned_directories,
-                        self.scan_result.file_count,
-                        humansize::format_size(self.scan_result.size, DECIMAL)
-                    ));
-                    ui.add(scanning_label);
-                }
+                clicked_index = PathBar::new(&self.analysis_result.data_stack).show(ui);
+                let scanning_label = Label::new(format!(
+                    "Directories: {}, Files: {}, Volume {}, scanning {}",
+                    self.scanned_directories,
+                    self.scan_result.file_count,
+                    humansize::format_size(self.scan_result.size, DECIMAL),
+                    self.scanning,
+                ));
+                ui.add(scanning_label);
                 AboutDialog::new(&mut self.about_open).show_button(ctx, ui);
             });
         });
