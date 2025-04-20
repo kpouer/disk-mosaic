8ca9b8f52105e5930c87323145c37fd9e1727d97
Matthieu Casanova <chocolat.mou@gmail.com>
2025-04-20 19:43:19 +0200

    add icon and minimum size

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,13 @@ use egui_extras::install_image_loaders;
 
 fn main() -> eframe::Result {
     env_logger::init();
-    let options = eframe::NativeOptions::default();
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_app_id("disk-mosaic")
+            .with_icon(icon_data())
+            .with_min_inner_size([320.0, 200.0]),
+        ..Default::default()
+    };
     eframe::run_native(
         "Disk Mosaic",
         options,
@@ -26,4 +32,22 @@ fn main() -> eframe::Result {
             Ok(Box::new(DiskAnalyzerApp::new(settings)))
         }),
     )
+}
+
+fn icon_data() -> egui::IconData {
+    let app_icon_png_bytes = include_bytes!("../media/icon.png");
+
+    match eframe::icon_data::from_png_bytes(app_icon_png_bytes) {
+        Ok(icon_data) => icon_data,
+        Err(err) => {
+            #[cfg(debug_assertions)]
+            panic!("Failed to load app icon: {err}");
+
+            #[cfg(not(debug_assertions))]
+            {
+                re_log::warn!("Failed to load app icon: {err}");
+                Default::default()
+            }
+        }
+    }
 }
