667f2c3cc10cfa62d36f165a9cfa9efb55a4e022
Matthieu Casanova <chocolat.mou@gmail.com>
2025-04-20 21:33:51 +0200

    Windows icon

---
+++ build.rs
@@ -0,0 +1,11 @@
+#[cfg(windows)]
+fn main() {
+    let mut res = winres::WindowsResource::new();
+    res.set_icon("media/img.png");
+    res.compile().unwrap();
+}
+
+#[cfg(not(windows))]
+fn main() {
+    // Do nothing on non-Windows platforms
+}
